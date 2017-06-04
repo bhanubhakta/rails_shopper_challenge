fca7f375e43470274b07b3ea89f0b66b722dc4ce
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-06-04 16:46:34 -0500

    Add error parser.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def parse_error(errors)
+    error = errors.first
+    error[0].to_s.split("_").join(" ").capitalize + " #{error[1]}"
+  end
+
+  def permitted_values
+    params.require(controller_name.singularize.to_sym).permit permitted_params
+  end
 end
