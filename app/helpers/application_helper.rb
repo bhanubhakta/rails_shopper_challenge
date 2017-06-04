b94e80c04c17ca6c052469a4494df89d7852fee0
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-06-04 16:25:53 -0500

    Add helpers for handling session login and logout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_applicant
+    @current_applicant ||= Applicant.find_by(email: session[:email])
+  end
+
+  def logged_in?
+    !current_applicant.nil?
+  end
+
+  def log_in(user)
+    session[:email] = user.email
+  end
+
+  def log_out
+    session[:email] = nil
+  end
 end
