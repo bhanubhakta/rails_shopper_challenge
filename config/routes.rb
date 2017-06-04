fa12584e8467558191d9c4585adf4d5c9f74c6be
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-06-04 16:30:46 -0500

    Add route for new register.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "applicants/sign_up" => "applicants#new", as: :register
   resources :applicants, only: [:create, :update, :show, :new]
   resources :funnels, only: [:index]
 end
