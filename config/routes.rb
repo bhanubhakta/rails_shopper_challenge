9b899d06d8163b24dc9c8cd9296642e34451713e
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-06-04 16:48:20 -0500

    Add routes for sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
   get "applicants/sign_up" => "applicants#new", as: :register
+  get "applicants/login" => "sessions#new", as: :login
+  post "applicants/login" => "sessions#create"
+  post "applicants/logout" => "sessions#logout", as: :logout
   resources :applicants, only: [:create, :update, :show, :new]
   resources :funnels, only: [:index]
 end
