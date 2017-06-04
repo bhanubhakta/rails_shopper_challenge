beed59556f4b964323d0ea17d3021ca337c1c571
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-06-04 17:11:48 -0500

    Add sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def create
+    applicant = Applicant.find_by(email: email)
+    if applicant && applicant.phone == phone
+      flash[:notice] = "Login successful."
+      log_in applicant
+      redirect_to edit_applicant_path(applicant)
+    else
+      flash[:alert] = "Incorrect email or phone."
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    log_out
+    redirect_to root_path
+  end
+
+  private
+
+  def email
+    @email ||= params[:email]
+  end
+
+  def phone
+    @phone ||= params[:phone]
+  end
+end
